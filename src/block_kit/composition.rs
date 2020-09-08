31ad36f4b89839bf853b991b804f4c84c0057465
David Buchan-Swanson <david.buchanswanson@gmail.com>
2020-09-08 21:22:30 +1000

    add tests for composition

--- src/block_kit/composition.rs
+++ src/block_kit/composition.rs
@@ -5,6 +5,27 @@ pub enum Text {
     Markdown(Markdown),
 }
 
+/// https://api.slack.com/reference/block-kit/composition-objects#text
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// let expected = json!({
+///   "type": "plain_text",
+///   "text": "This is some plain text"
+/// });
+///
+/// let text = PlainText::builder().text("This is some plain text").build();
+/// let output = serde_json::to_value(&text).unwrap();
+/// assert_json_eq!(&expected, output);
+///
+/// // Also implements From<&str>
+/// let text: PlainText = "This is some plain text".into();
+/// let output = serde_json::to_value(&text).unwrap();
+/// assert_json_eq!(&expected, output);
+/// ```
 #[impl_for(Text)]
 #[derive(TypedBuilder, Serialize)]
 pub struct PlainText {
@@ -24,6 +45,27 @@ impl From<&str> for PlainText {
     }
 }
 
+/// https://api.slack.com/reference/block-kit/composition-objects#text
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// let expected = json!({
+///   "type": "mrkdwn",
+///   "text": "*Some* _formatted_ ~text~"
+/// });
+///
+/// let text = Markdown::builder().text("*Some* _formatted_ ~text~").build();
+/// let output = serde_json::to_value(&text).unwrap();
+/// assert_json_eq!(&expected, output);
+///
+/// // Also implements From<&str>
+/// let text: Markdown = "*Some* _formatted_ ~text~".into();
+/// let output = serde_json::to_value(&text).unwrap();
+/// assert_json_eq!(&expected, output);
+/// ```
 #[impl_for(Text)]
 #[derive(TypedBuilder, Serialize)]
 pub struct Markdown {
@@ -49,12 +91,42 @@ pub enum ConfirmationStyle {
     Danger,
 }
 
-impl Default for ConfirmationStyle {
-    fn default() -> Self {
-        Self::Primary
-    }
-}
-
+/// https://api.slack.com/reference/block-kit/composition-objects#confirm
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+/// let confirm = Confirmation::builder()
+///   // this is a `PlainText`, but we got `impl From<&str>`
+///   .title("Are you sure?")
+///   .text(Markdown::builder().text("Wouldn't you prefer a good game of _chess_?").build())
+///   .confirm("Do it")
+///   .deny("Stop, I've changed my mind!")
+///   // if you need to, you can specify a style:
+///   // .style(ConfirmationStyle::Primary)
+///   .build();
+///   let expected = json!({
+///     "title": {
+///         "type": "plain_text",
+///         "text": "Are you sure?"
+///     },
+///     "text": {
+///         "type": "mrkdwn",
+///         "text": "Wouldn't you prefer a good game of _chess_?"
+///     },
+///     "confirm": {
+///         "type": "plain_text",
+///         "text": "Do it"
+///     },
+///     "deny": {
+///         "type": "plain_text",
+///         "text": "Stop, I've changed my mind!"
+///     }
+///   });
+/// let output = serde_json::to_value(&confirm).unwrap();
+/// assert_json_eq!(output, expected);
+/// ```
 #[derive(TypedBuilder, Serialize)]
 pub struct Confirmation {
     #[builder(setter(into))]
@@ -66,10 +138,33 @@ pub struct Confirmation {
     #[builder(setter(into))]
     deny: PlainText,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(strip_option))]
+    #[builder(setter(strip_option), default)]
     style: Option<ConfirmationStyle>,
 }
 
+/// https://api.slack.com/reference/block-kit/composition-objects#option
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// let expected = json!({
+///   "text": {
+///       "type": "plain_text",
+///       "text": "Maru"
+///   },
+///   "value": "maru"
+/// });
+///
+/// let option = PlainTextOptionItem::builder()
+///     .text("Maru")
+///     .value("maru")
+///     .build();
+///
+/// let output = serde_json::to_value(&option).unwrap();
+/// assert_json_eq!(expected, output);
+/// ```
 // Used in select and multi-select
 #[derive(TypedBuilder, Serialize)]
 pub struct PlainTextOptionItem {
@@ -78,11 +173,34 @@ pub struct PlainTextOptionItem {
     #[builder(setter(into))]
     value: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(into, strip_option))]
+    #[builder(setter(into, strip_option), default)]
     description: Option<PlainText>,
 }
 
-// Used in Radio buttons and Checkboxes
+/// https://api.slack.com/reference/block-kit/composition-objects#option
+/// For use in Radio buttons and Checkboxes
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// let expected = json!({
+///   "text": {
+///       "type": "mrkdwn",
+///       "text": "Maru"
+///   },
+///   "value": "maru"
+/// });
+///
+/// let option = MarkdownOptionItem::builder()
+///     .text("Maru")
+///     .value("maru")
+///     .build();
+///
+/// let output = serde_json::to_value(&option).unwrap();
+/// assert_json_eq!(expected, output);
+/// ```
 #[derive(TypedBuilder, Serialize)]
 pub struct MarkdownOptionItem {
     #[builder(setter(into))]
@@ -90,12 +208,37 @@ pub struct MarkdownOptionItem {
     #[builder(setter(into))]
     value: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(into, strip_option))]
+    #[builder(setter(into, strip_option), default)]
     description: Option<PlainText>,
 }
 
-// Overflow options have this extra `url` option, otherwise they're the same as
-// the PlainTextOptionItem
+/// https://api.slack.com/reference/block-kit/composition-objects#option
+/// Overflow options have this extra `url` option, otherwise they're the same as
+/// the PlainTextOptionItem
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// let expected = json!({
+///   "text": {
+///       "type": "plain_text",
+///       "text": "Maru"
+///   },
+///   "value": "maru",
+///   "url": "https://example.com"
+/// });
+///
+/// let option = OverflowOptionItem::builder()
+///     .text("Maru")
+///     .value("maru")
+///     .url("https://example.com")
+///     .build();
+///
+/// let output = serde_json::to_value(&option).unwrap();
+/// assert_json_eq!(expected, output);
+/// ```
 #[derive(TypedBuilder, Serialize)]
 pub struct OverflowOptionItem {
     #[builder(setter(into))]
@@ -103,15 +246,102 @@ pub struct OverflowOptionItem {
     #[builder(setter(into))]
     value: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(into, strip_option))]
+    #[builder(setter(into, strip_option), default)]
     description: Option<PlainText>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(into, strip_option))]
+    #[builder(setter(into, strip_option), default)]
     url: Option<String>,
 }
 
-// These are type-accurate assuming that Option Groups continue to only be
-// allowed in selects and multi-selects.
+/// https://api.slack.com/reference/block-kit/composition-objects#option_group
+/// These only work for select + multi select, so they only support plaintext
+/// `options`.
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// // This one's pretty long :grimacing:
+/// let expected = json!([
+///   {
+///     "label": {
+///       "type": "plain_text",
+///       "text": "Group 1"
+///     },
+///     "options": [
+///       {
+///         "text": {
+///             "type": "plain_text",
+///             "text": "*this is plain_text text*"
+///         },
+///         "value": "value-0"
+///       },
+///       {
+///         "text": {
+///             "type": "plain_text",
+///             "text": "*this is plain_text text*"
+///         },
+///         "value": "value-1"
+///       },
+///       {
+///         "text": {
+///             "type": "plain_text",
+///             "text": "*this is plain_text text*"
+///         },
+///         "value": "value-2"
+///       }
+///     ]
+///   },
+///   {
+///     "label": {
+///         "type": "plain_text",
+///         "text": "Group 2"
+///     },
+///     "options": [
+///       {
+///         "text": {
+///             "type": "plain_text",
+///             "text": "*this is plain_text text*"
+///         },
+///         "value": "value-3"
+///       }
+///     ]
+///   }
+/// ]);
+///
+/// let option = vec![
+///   OptionGroup::builder()
+///     .label("Group 1")
+///     .options(vec![
+///       PlainTextOptionItem::builder()
+///         .text("*this is plain_text text*")
+///         .value("value-0")
+///         .build(),
+///       PlainTextOptionItem::builder()
+///         .text("*this is plain_text text*")
+///         .value("value-1")
+///         .build(),
+///       PlainTextOptionItem::builder()
+///         .text("*this is plain_text text*")
+///         .value("value-2")
+///         .build(),
+///     ])
+///     .build(),
+///   OptionGroup::builder()
+///     .label("Group 2")
+///     .options(vec![
+///       PlainTextOptionItem::builder()
+///         .text("*this is plain_text text*")
+///         .value("value-3")
+///         .build(),
+///     ])
+///     .build(),
+/// ];
+///
+/// let output = serde_json::to_value(&option).unwrap();
+/// assert_json_eq!(expected, output);
+/// ```
 #[derive(TypedBuilder, Serialize)]
 pub struct OptionGroup {
     #[builder(setter(into))]
@@ -120,6 +350,7 @@ pub struct OptionGroup {
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum FilterInclusions {
     IM,
     MPIM,
@@ -127,17 +358,38 @@ pub enum FilterInclusions {
     Public,
 }
 
-// These are all technically optional, but at least one of the 3 must be set. I
-// don't have a way to model this in the type system currently
+/// https://api.slack.com/reference/block-kit/composition-objects#filter_conversations
+///
+/// These are all technically optional, but at least one of the 3 must be set. I
+/// don't have a way to model this in the type system currently
+///
+/// ```
+/// # use assert_json_diff::assert_json_eq;
+/// # use serde_json::json;
+/// # use slack_lib::block_kit::composition::*;
+///
+/// let expected = json!({
+///   "include": ["public", "mpim"],
+///   "exclude_bot_users": true
+/// });
+///
+/// let filter = Filter::builder()
+///     .include(vec![FilterInclusions::Public, FilterInclusions::MPIM])
+///     .exclude_bot_users(true)
+///     .build();
+///
+/// let output = serde_json::to_value(&filter).unwrap();
+/// assert_json_eq!(expected, output);
+/// ```
 #[derive(TypedBuilder, Serialize)]
 pub struct Filter {
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(strip_option))]
+    #[builder(setter(strip_option), default)]
     include: Option<Vec<FilterInclusions>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(strip_option))]
+    #[builder(setter(strip_option), default)]
     exclude_external_shared_channels: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[builder(setter(strip_option))]
+    #[builder(setter(strip_option), default)]
     exclude_bot_users: Option<bool>,
 }
